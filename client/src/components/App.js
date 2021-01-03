8fd565742f10e4b30fdcf71e07853ab973ff13df
Oxana Odegova <oxyapple@yandex.ru>
2021-01-03 14:31:59 +0100

    add new components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,34 +1,24 @@
 import React from 'react';
-import {MemoryRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo">Navigate to Page Two</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <button>Click Me!</button>
-      <Link to="/">Navigate to Page One</Link>
-    </div>
-  );
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamList from './streams/StreamList';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
     <div>
-      <MemoryRouter>
+      <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
         </div>
-      </MemoryRouter>
+      </BrowserRouter>
     </div>
   );
 };
